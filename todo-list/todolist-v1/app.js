b78cdc5ac317a54afd9afc7872f0f67a2a900f1b
Jamshid-Ganiev <jamesganiev227@gmail.com>
2023-06-18 23:57:49 +0900

    todo list with EJS v1

---
+++ todo-list/todolist-v1/app.js
@@ -0,0 +1,53 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// Embedded JavaScript Server
+app.set("view engine", "ejs");
+
+app.get('/', function (req, res){
+
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+
+    switch (currentDay) {
+        case 0:
+            day = "Sunday"
+            break;
+        case 1:
+            day = "Monday"
+            break;
+        case 2:
+            day = "Tuesday"
+            break;
+        case 3:
+            day = "Wednesday"
+            break;
+        case 4:
+            day = "Thursday"
+            break;
+        case 5:
+            day = "Friday"
+            break;
+        case 6:
+            day = "Saturday"
+            break;
+        default:
+        console.log("Error: Current day is equal to" + currentDay);
+
+    }
+
+    res.render("list", {
+        kindOfDay: day
+    });
+});
+
+
+app.listen(8000, function(){
+    console.log("Server started on port:8000");
+});
+
