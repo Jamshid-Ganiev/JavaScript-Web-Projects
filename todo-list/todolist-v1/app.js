01e5ca9131321aa205f139c4e668ee8e7c3b0400
Jamshid-Ganiev <jamesganiev227@gmail.com>
2023-06-19 01:16:05 +0900

    todo app with EJS v2

--- todo-list/todolist-v1/app.js
+++ todo-list/todolist-v1/app.js
@@ -5,45 +5,37 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let items = [];
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 // Embedded JavaScript Server
 app.set("view engine", "ejs");
 
+
 app.get('/', function (req, res){
 
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
-
-    switch (currentDay) {
-        case 0:
-            day = "Sunday"
-            break;
-        case 1:
-            day = "Monday"
-            break;
-        case 2:
-            day = "Tuesday"
-            break;
-        case 3:
-            day = "Wednesday"
-            break;
-        case 4:
-            day = "Thursday"
-            break;
-        case 5:
-            day = "Friday"
-            break;
-        case 6:
-            day = "Saturday"
-            break;
-        default:
-        console.log("Error: Current day is equal to" + currentDay);
-
+    let today = new Date();
+    let currentDay = today.getDay();
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
     }
 
+    let day = today.toLocaleDateString("en-US", options);
+
     res.render("list", {
-        kindOfDay: day
+        kindOfDay: day,
+        newListItems: items
     });
+});
+
+app.post("/", function(req, res){
+    const item = req.body.newItem;
+    items.push(item)
+
+    res.redirect("/");
 });
 
 
